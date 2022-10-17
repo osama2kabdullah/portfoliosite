99af60d68f7febe7ac907ffaaa25d27402a0c3aa
osama-abdullah <osama2kabdullah@gmail.com>
2022-10-17 21:12:36 +0600

    emailjs used for reciev emails from contact and feedback

--- src/pages/projects/FeedBack.js
+++ src/pages/projects/FeedBack.js
@@ -1,14 +1,32 @@
+import EmailJSResponseStatus from "@emailjs/browser";
 import React from "react";
 
 const FeedBack = () => {
   const inputStyle = "lg:p-3 p-2 lg:text-xl text-md rounded-lg";
+
+  const sendfeedback = (e) => {
+    e.preventDefault();
+    EmailJSResponseStatus.sendForm(
+      "service_9grfslc",
+      "template_fo9dsae",
+      e.target,
+      "bjpPddRjt83gulhRB"
+    )
+      .then((res) => {
+        if (res.status === 200) {
+          e.target.reset();
+        }
+      })
+      .then((err) => err && alert(err));
+  };
+
   return (
     <section className="mt-12">
       <div className="border-t-2 py-3">
         <h1 className="text-3xl text-center font-primary  text-gray-500">
           Feedback
         </h1>
-        <form className="grid lg:gap-12 gap-5  mb-12">
+        <form onSubmit={sendfeedback} className="grid lg:gap-12 gap-5  mb-12">
           <div className="grid lg:w-3/6">
             <label htmlFor="email" className="lg:text-xl text-lg mb-3">
               Email
@@ -27,7 +45,7 @@ const FeedBack = () => {
             </label>
             <textarea
               className={inputStyle}
-              name="message"
+              name="feedback"
               id=""
               rows="3"
             ></textarea>
